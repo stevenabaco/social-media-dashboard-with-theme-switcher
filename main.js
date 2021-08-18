86760ea7b7c0c309eafec37b5451cad1d277c2c0
Steven Abaco <stevenabaco@gmail.com>
2021-08-17 23:02:52 -0500

    Update mode description adjust margin top

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const input = document.querySelector('#switch input');
+const theme = document.querySelector('#theme-switcher h2');
 
-input.addEventListener('change', (e) => {
-  if (e.target.checked) {
-    document.body.setAttribute(('data-theme'), 'dark');
-  } else {
-    document.body.setAttribute('data-theme', 'light');
-  }
-})
+input.addEventListener('change', e => {
+	if (e.target.checked) {
+		document.body.setAttribute('data-theme', 'dark');
+		theme.textContent = 'Dark Mode';
+	} else {
+		document.body.setAttribute('data-theme', 'light');
+		theme.textContent = 'Light Mode';
+	}
+});
